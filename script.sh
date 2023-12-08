cbac1bfcc6bc57b099738cdd16eccfe317cd0ec6
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2023-12-08 11:18:50 +0000

    script 2

--- script.sh
+++ script.sh
@@ -5,3 +5,7 @@ echo "creating pod"
 kubectl create -f create-pod 
 echo "creating Replicaset"
 kubectl create -f create-Replicaset
+echo "creting depolyement"
+kubectl create -f Deployement
+echo "creating service nodeport"
+kubectl create -f Service
